e8fb901b6003ab61573d2c8396ac37f03f1f2e14
Nikita Kaberov <enagtim@mail.ru>
2024-10-24 13:13:35 +0300

    Добавлен outlet

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,25 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Menu } from './pages/Menu/Menu';
+import {LayoutMenu} from './layout/LayoutMenu/LayoutMenu';
 import { Cart } from './pages/Cart/Cart';
 import { Error } from './pages/Error/Error';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 const router = createBrowserRouter([
 	{
 		path: '/', 
-		element: <Menu/>
+		element: <LayoutMenu/>,
+		children: [
+			{
+				path: '/', 
+				element: <Menu/>
+			}, 
+			{
+				path: '/cart',
+				element: <Cart/>
+			}
+		]
 	}, 
-	{
-		path: '/cart',
-		element: <Cart/>
-	},
 	{
 		path: '*',
 		element: <Error/>
