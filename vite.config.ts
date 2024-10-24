2ef946ec150f365106cd6a17baf37aa8af288601
Nikita Kaberov <enagtim@mail.ru>
2024-10-24 12:10:22 +0300

    Сделаны базовые компоненты

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "localhost",
-    port: 3003,
-    open: true,
-    watch: {
-      usePolling: true,
-    },
-    hmr: {
-      protocol: "ws",
-      host: "localhost",
-    },
-  },
-})
+	plugins: [react()],
+	server: {
+		host: 'localhost',
+		port: 3003,
+		open: true,
+		watch: {
+			usePolling: true
+		},
+		hmr: {
+			protocol: 'ws',
+			host: 'localhost'
+		}
+	}
+});
