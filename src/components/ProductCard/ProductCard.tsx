2ba8f395b456b4a6ef66a6fe8eafa6cd7821d2e8
Nikita Kaberov <enagtim@mail.ru>
2024-10-25 11:18:57 +0300

    Сделана карточка товара

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import { ProductCardProps } from './Product.props';
+import styles from './ProductCard.module.css';
+function ProductCard ( props : ProductCardProps) {
+	return (
+		<Link to={'/'} className={styles['link']}>
+			<div className={styles['card']}>
+				<div className={styles['head']} style={{backgroundImage: `url('${props.image}')`}}>
+					<div className={styles['price']}>
+						{props.price}&nbsp;
+						<span className={styles['currency']}>₽</span>
+					</div>
+					<button className={styles['add-to-cart']}>
+						<img src="/favorite-icon.svg" alt="Добавить в корзину" />
+					</button>
+					<div className={styles['rating']}>{props.rating}&nbsp;
+						<img src="/star-icon.svg" alt="Иконка звезд" />
+					</div>
+				</div>
+				<div className={styles['footer']}>
+					<div className={styles['title']}>{props.title}</div>
+					<div className={styles['desription']}>{props.description}</div>
+				</div>
+			</div>
+		</Link>
+		
+	);
+}
+export default ProductCard;
